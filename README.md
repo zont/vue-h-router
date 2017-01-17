df72f69187c8078781ef212635402952af12cacd
alexander.zonov <alexander.zonov@developonbo.ru>
2017-01-17 16:23:04 +0300

    v0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
 # vue-h-router
+
+### Introduction
+
+`vue-h-router` is the small router for [Vue.js](http://vuejs.org). It deeply integrates with Vue.js core to make building Single Page Applications with Vue.js a breeze. Features include:
+
+- Modular, component-based router configuration
+- Route params
+- Hash mode navigation
+- HTML5 history support
+- Route hooks
+
+### Setup
+
+``` bash
+npm install vue-h-router
+```
+
+### Example
+
+``` javascript
+import Vue from 'vue';
+import router from 'vue-h-router';
+import LoginView from './views/login';
+import HomeView from './views/home';
+
+
+Vue.use(router);
+
+
+const user = {
+  auth: false;
+};
+const checkAuth = () => {
+  return user.auth ? Promise.resolve() : Promise.reject();
+};
+const checkNotAuth = () => {
+  return !user.auth ? Promise.resolve() : Promise.reject();
+};
+const app = new Vue({
+  template: `
+    <div class="main">
+      <component :is="$route.component"/>
+    </div>
+  `,
+  router,
+  el: '#app'
+});
+
+router.init([
+  {
+    url: '/login',
+    component: LoginView,
+    before: checkNotAuth
+  },
+  {
+    url: '/home',
+    component: HomeView,
+    before: checkAuth
+  },
+  {
+    url: '.*',
+    redirect() {
+      return user.auth ? `/home` : '/login';
+    }
+  }
+]);
+```
+
+
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
---
+++ index.js
@@ -0,0 +1,101 @@
+export default {
+  routes: [],
+
+  route: {
+    url: '',
+    args: [],
+    component: {}
+  },
+
+  init(routes) {
+    this.routes = routes.map(route => {
+      route.url = typeof route.url === 'string' ? new RegExp(`^${route.url}$`) : route.url;
+      return route;
+    });
+
+    this.parse(location.hash);
+  },
+
+  redirect(url) {
+    history.replaceState({}, '', `#${url}`);
+    this.parse(location.hash);
+  },
+
+  parse(url, routes = this.routes) {
+    const path = url.replace(/^[^#]*#/, '');
+    const route = routes.find(route => route.url.test(path));
+
+    if (route) {
+      const newRoute = {
+        url: path,
+        args: [...route.url.exec(path).slice(1)],
+        component: route.component
+      };
+
+      if (route.redirect) {
+        if (typeof route.redirect === 'function') {
+          if (route.redirect instanceof Promise) {
+            route.redirect(newRoute)
+              .then(url => this.redirect(url))
+              .catch(e => {
+                console.error(e);
+                history.back();
+              });
+          } else {
+            this.redirect(route.redirect(newRoute));
+          }
+        } else {
+          this.redirect(route.redirect);
+        }
+      } else if (route.before) {
+        route.before(newRoute)
+          .then(() => this.setRoute(newRoute, route))
+          .catch(() => this.parse(url, routes.slice(routes.indexOf(route) + 1)));
+      } else {
+        this.setRoute(newRoute, route);
+      }
+    } else {
+      this.setRoute({
+        url: path,
+        args: [],
+        component: {}
+      });
+    }
+  },
+
+  setRoute(route, _route) {
+    const oldRoute = this.route;
+    this.app._route = this.route = route;
+    if (_route && typeof _route.after === 'function') {
+      _route.after(oldRoute, route);
+    }
+  },
+
+  install(Vue) {
+    const start = app => {
+      this.app = app;
+      window.addEventListener('hashchange', e => this.parse(e.newURL));
+    };
+
+    Object.defineProperty(Vue.prototype, '$router', {
+      get() {
+        return this.$root._router;
+      }
+    });
+    Object.defineProperty(Vue.prototype, '$route', {
+      get() {
+        return this.$root._route;
+      }
+    });
+
+    Vue.mixin({
+      beforeCreate() {
+        if (this.$options.router) {
+          this._router = this.$options.router;
+          Vue.util.defineReactive(this, '_route', this._router.route);
+          start(this);
+        }
+      }
+    });
+  }
+};
