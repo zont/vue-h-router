69ba12188a63cc416f287b2be4b8038042d302a4
Alexander Zonov <alexander.zonov@developonbox.ru>
2017-11-25 14:26:30 +0300

    Fixed tests

--- __tests__/router-spec.js
+++ __tests__/router-spec.js
@@ -1,7 +1,6 @@
 import Vue from 'vue/dist/vue.min';
 import router from '../index';
 
-
 class Helper {
   constructor() {
     this._callback = () => {};
@@ -20,7 +19,6 @@ const smallTimeout = 10;
 const bigTimeout = 100;
 const wait = timeout => new Promise(resolve => setTimeout(resolve, timeout));
 
-
 describe('router', () => {
   it('defined', () => {
     expect(router.install).toBeDefined();
@@ -29,9 +27,7 @@ describe('router', () => {
 
     Vue.use(router);
 
-    const app = new Vue({
-      template: '<div></div>'
-    });
+    const app = new Vue({ template: '<div></div>' });
 
     expect(app.$router).toBeDefined();
     expect(app.$route).toBeDefined();
@@ -43,227 +39,155 @@ describe('router', () => {
     expect(app.$route.component).toBeDefined();
   });
 
-  it('init', done => {
+  it('init', async () => {
+    expect.assertions(8);
+
     const routes = [
-      {
-        url: '/init/home',
-        component: {
-          template: '<div></div>'
-        }
-      },
-      {
-        url: '/init/about',
-        component: {
-          template: '<span></span>'
-        }
-      }
+      { url: '/init/home', component: { template: '<div></div>' } },
+      { url: '/init/about', component: { template: '<span></span>' } }
     ];
 
-    helper.goTo('/init/about')
-      .then(() => {
-        expect(router.route.url).toEqual('/init/about');
-        expect(router.route.component).toEqual(null);
-        return helper.goTo('/init/home');
-      })
-      .then(() => {
-        expect(router.route.url).toEqual('/init/home');
-        expect(router.route.component).toEqual(null);
-        router.init(routes);
-        expect(router.route.component.template).toEqual(routes[0].component.template);
+    await helper.goTo('/init/about');
 
-        return helper.goTo('/init/about');
-      })
-      .then(() => {
-        expect(router.route.url).toEqual('/init/about');
-        expect(router.route.component.template).toEqual(routes[1].component.template);
-        router.init([]);
-        expect(router.route.component).toEqual(null);
-      })
-      .then(done)
-      .catch(done.fail);
+    expect(router.route.url).toEqual('/init/about');
+    expect(router.route.component).toEqual(null);
+
+    await helper.goTo('/init/home');
+
+    expect(router.route.url).toEqual('/init/home');
+    expect(router.route.component).toEqual(null);
+
+    router.init(routes);
+
+    expect(router.route.component.template).toEqual(routes[0].component.template);
+
+    await helper.goTo('/init/about');
+
+    expect(router.route.url).toEqual('/init/about');
+    expect(router.route.component.template).toEqual(routes[1].component.template);
+
+    router.init([]);
+
+    expect(router.route.component).toEqual(null);
   });
 
   describe('routes', () => {
-    it('url', done => {
+    it('url', async () => {
+      expect.assertions(4);
+
       const routes = [
-        {
-          url: '/routes/url/home',
-          component: {
-            template: '<div></div>'
-          }
-        },
-        {
-          url: /^\/routes\/url\/about$/,
-          component: {
-            template: '<span></span>'
-          }
-        }
+        { url: '/routes/url/home', component: { template: '<div></div>' } },
+        { url: /^\/routes\/url\/about$/, component: { template: '<span></span>' } }
       ];
 
       router.init(routes);
 
-      helper.goTo('/routes/url/about')
-        .then(() => {
-          expect(router.route.url).toEqual('/routes/url/about');
-          expect(router.route.component.template).toEqual(routes[1].component.template);
-          return helper.goTo('/routes/url/home');
-        })
-        .then(() => {
-          expect(router.route.url).toEqual('/routes/url/home');
-          expect(router.route.component.template).toEqual(routes[0].component.template);
-        })
-        .then(done)
-        .catch(done.fail);
+      await helper.goTo('/routes/url/about');
+
+      expect(router.route.url).toEqual('/routes/url/about');
+      expect(router.route.component.template).toEqual(routes[1].component.template);
+
+      await helper.goTo('/routes/url/home');
+
+      expect(router.route.url).toEqual('/routes/url/home');
+      expect(router.route.component.template).toEqual(routes[0].component.template);
     });
 
-    it('args', done => {
+    it('args', async () => {
+      expect.assertions(5);
+
       const routes = [
-        {
-          url: '/routes/args/user/(\\d+)',
-          component: {
-            template: '<div></div>'
-          }
-        },
-        {
-          url: /^\/routes\/args\/department\/(\d+)\?option=(\w+)$/,
-          component: {
-            template: '<span></span>'
-          }
-        }
+        { url: '/routes/args/user/(\\d+)', component: { template: '<div></div>' } },
+        { url: /^\/routes\/args\/department\/(\d+)\?option=(\w+)$/, component: { template: '<span></span>' } }
       ];
 
       router.init(routes);
 
-      helper.goTo('/routes/args/user/11')
-        .then(() => {
-          expect(router.route.args.length).toBe(1);
-          expect(router.route.args[0]).toEqual('11');
-          return helper.goTo('/routes/args/department/456?option=showAll');
-        })
-        .then(() => {
-          expect(router.route.args.length).toBe(2);
-          expect(router.route.args[0]).toEqual('456');
-          expect(router.route.args[1]).toEqual('showAll');
-        })
-        .then(done)
-        .catch(done.fail);
+      await helper.goTo('/routes/args/user/11');
+
+      expect(router.route.args.length).toBe(1);
+      expect(router.route.args[0]).toEqual('11');
+
+      await helper.goTo('/routes/args/department/456?option=showAll');
+
+      expect(router.route.args.length).toBe(2);
+      expect(router.route.args[0]).toEqual('456');
+      expect(router.route.args[1]).toEqual('showAll');
     });
 
-    it('redirect', done => {
+    it('redirect', async () => {
+      expect.assertions(3);
+
       const routes = [
-        {
-          url: '/routes/redirect/user',
-          redirect: '/routes/redirect/person'
-        },
-        {
-          url: '/routes/redirect/person',
-          component: {}
-        },
-        {
-          url: '/routes/redirect/department',
-          redirect() {
-            return Promise.resolve('/routes/redirect/it');
-          }
-        },
-        {
-          url: '/routes/redirect/it',
-          component: {}
-        },
-        {
-          url: '/routes/redirect/some',
-          redirect() {
-            return Promise.reject();
-          }
-        }
+        { url: '/routes/redirect/user', redirect: '/routes/redirect/person' },
+        { url: '/routes/redirect/person', component: {} },
+        { url: '/routes/redirect/department', redirect: () => Promise.resolve('/routes/redirect/it') },
+        { url: '/routes/redirect/it', component: {} },
+        { url: '/routes/redirect/some', redirect: () => Promise.reject() }
       ];
 
       router.init(routes);
 
-      helper.goTo('/routes/redirect/user')
-        .then(() => {
-          expect(router.route.url).toEqual('/routes/redirect/person');
-          return helper.goTo('/routes/redirect/department');
-        })
-        .then(() => wait(smallTimeout))
-        .then(() => {
-          expect(router.route.url).toEqual('/routes/redirect/it');
-          return helper.goTo('/routes/redirect/some');
-        })
-        .then(() => wait(bigTimeout))
-        .then(() => {
-          expect(router.route.url).toEqual('/routes/redirect/it');
-        })
-        .then(done)
-        .catch(done.fail);
+      await helper.goTo('/routes/redirect/user');
+      await wait(smallTimeout);
+
+      expect(router.route.url).toEqual('/routes/redirect/person');
+
+      await helper.goTo('/routes/redirect/department');
+      await wait(smallTimeout);
+
+      expect(router.route.url).toEqual('/routes/redirect/it');
+
+      await helper.goTo('/routes/redirect/some');
+      await wait(bigTimeout);
+
+      expect(router.route.url).toEqual('/routes/redirect/it');
     });
 
-    it('before', done => {
+    it('before', async () => {
+      expect.assertions(3);
+
       const routes = [
-        {
-          url: '/routes/before/user/.+',
-          before() {
-            return Promise.reject();
-          }
-        },
-        {
-          url: '/routes/before/user/12',
-          component: {
-            template: '<div>User 12</div>'
-          }
-        },
+        { url: '/routes/before/user/.+', before: () => Promise.reject() },
+        { url: '/routes/before/user/12', component: { template: '<div>User 12</div>' } },
         {
           url: '/routes/before/user/(\\d+)',
-          component: {
-            template: '<div>User with timeout > 100</div>'
-          },
+          component: { template: '<div>User with timeout > 100</div>' },
           before(newRoute) {
             return Number(newRoute.args[0]) > bigTimeout ? Promise.resolve() : Promise.reject();
           }
         },
-        {
-          url: '/routes/before/.*',
-          component: {
-            template: '<div>Default route</div>'
-          }
-        }
+        { url: '/routes/before/.*', component: { template: '<div>Default route</div>' } }
       ];
 
       router.init(routes);
 
-      helper.goTo('/routes/before/user/12')
-        .then(() => wait(smallTimeout))
-        .then(() => {
-          expect(router.route.component.template).toEqual(routes[1].component.template);
-          return helper.goTo('/routes/before/user/13');
-        })
-        .then(() => wait(smallTimeout))
-        .then(() => {
-          expect(router.route.component.template).toEqual(routes[3].component.template);
-          return helper.goTo('/routes/before/user/123');
-        })
-        .then(() => wait(smallTimeout))
-        .then(() => {
-          expect(router.route.component.template).toEqual(routes[2].component.template);
-        })
-        .then(done)
-        .catch(done.fail);
+      await helper.goTo('/routes/before/user/12');
+      await wait(smallTimeout);
+
+      expect(router.route.component.template).toEqual(routes[1].component.template);
+
+      await helper.goTo('/routes/before/user/13');
+      await wait(smallTimeout);
+
+      expect(router.route.component.template).toEqual(routes[3].component.template);
+
+      await helper.goTo('/routes/before/user/123');
+      await wait(smallTimeout);
+
+      expect(router.route.component.template).toEqual(routes[2].component.template);
     });
 
-    it('after', done => {
+    it('after', async () => {
+      expect.assertions(7);
+
       const url1 = '/routes/after/home/123';
       const url2 = '/routes/after/user/smith';
       const routes = [
-        {
-          url: '/routes/after/home/(.*)',
-          component: {
-            template: '<div>Home</div>'
-          }
-        },
+        { url: '/routes/after/home/(.*)', component: { template: '<div>Home</div>' } },
         {
           url: '/routes/after/user/(\\w+)',
-          component: {
-            template: '<div>User</div>'
-          },
+          component: { template: '<div>User</div>' },
           after(oldRoute, newRoute) {
             expect(oldRoute.url).toEqual(url1);
             expect(oldRoute.args[0]).toEqual('123');
@@ -278,10 +202,8 @@ describe('router', () => {
 
       router.init(routes);
 
-      helper.goTo(url1)
-        .then(() => helper.goTo(url2))
-        .then(done)
-        .catch(done.fail);
+      await helper.goTo(url1);
+      await helper.goTo(url2);
     });
   });
 });
